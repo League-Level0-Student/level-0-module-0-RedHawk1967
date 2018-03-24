a74a1ebdec1a388acd2c35a4f35af0545980bbff
League Student <league@iMac-4.attlocal.net>
2018-03-23 18:02:33 -0700

    0
    )))))0

---
+++ src/Everythingisawesome.java
@@ -0,0 +1,48 @@
+import javax.swing.JOptionPane;
+
+public class Everythingisawesome {
+public static void main(String[] args) {
+
+	
+ String thing = JOptionPane.showInputDialog("What Do you like");
+JOptionPane.showMessageDialog(null,thing+" is awesome");
+
+
+
+
+
+
+
+
+
+
+
+
+}
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/KnockKnockHaHa.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+
+public class KnockKnockHaHa {
+	public static void main(String[] args) {
+
+		JOptionPane.showMessageDialog(null,
+				"Knock, knock\n" + "Who’s there?\n" + "Iva.\n" + "Iva who?\n" + "I’ve a sore hand from knocking!");
+
+	}
+}
